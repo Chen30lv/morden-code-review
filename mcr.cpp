02fedcdd0f2cb81516ae370658429001ab08884e
Chen30lv <guanchenlv1230@163.com>
2023-09-11 20:48:39 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -1,3 +1,7 @@
+#include "iostream"
+
+using namespace std;
+
 bool isWin(char game[3][3]){
     bool win = false;
     if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
@@ -15,19 +19,35 @@ bool isWin(char game[3][3]){
 
 int main(){
     int i, j;
-    char game[3][3] = {' '}; // Tic-tac-toe
-    char player1 = 'X';
-    char player2 = 'O';
+    i = -1;
+    j = -1;
+    char game[3][3] = {'0'}; // Tic-tac-toe
+    char c;
     bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first.
     cout << "X = Player 1" << endl << "O = Player 2" << endl;
     for (int n=0; n<9; n++){
         turn = !turn;  // use the not-operator to change true to false or false to true.
-        if (turn == false)
+        if (turn == false){
             cout << "Player 1: ";
-        else
+        }
+        else{
             cout << "Player 2: ";
-        cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+        }
+
+        rewrite:
+        cout << "Which cell to mark? i:[0..2], j:[0..2]: ";
         cin >> i >> j;
+
+        if(game[i][j] != 'X' && game[i][j] != 'O' && i <= 2 && i >= 0 && j <= 2 && j >= 0) { //check if the input is appropriate.
+            goto next; //if the input is appropriate, go next
+        }
+        else{
+            cout << "You can not mark there." << endl;
+            while ((c = getchar()) != '\n');
+            goto rewrite; //if the input is not apropriate, ask for input again.
+        }
+
+        next:
         if (turn == false)
             game[i][j] = 'X';
         else
